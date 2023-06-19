8b236f032b929e81898e79389b9d2a6678ae12f9
skaunov <skaunov@disroot.org>
2023-06-19 16:41:53 -0400

    fixes for vercel

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   // reactStrictMode: true,
   // swcMinify: true,
+  webpack: (config, { isServer }) => {
+    // Add the polyfill to the client-side webpack config
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "stream": require.resolve("stream-browserify"),
+        "stream/web": require.resolve("stream-browserify"),
+        "stream-transform": require.resolve("web-streams-polyfill/dist/ponyfill.es2018.js"),
+      };
+    }
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
